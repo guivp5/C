98cc51b00d596e8a709cbdbb0399a60ed46235a8
Guilherme Vieira <134512885+guivp5@users.noreply.github.com>
2024-05-19 22:06:02 -0300

    eh

---
+++ exercicios/zaiatz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    
+    int X, i;
+    char letra[50];
+
+    printf("");
+    scanf("");
+
+    for (i = 0; i <= 3; i++)
+    {
+        if (X = 'A')
+        {
+            printf("!RESPOSTA CORRETA!");
+        } 
+        else {
+            printf("!RESPOSTA INCORRETA!");
+        }
+        printf("TENTE DE NOVO");
+        printf("Quanto é o resultado de 1 + 1?");
+        
+    }
+    
+
+    return 0;
+}
